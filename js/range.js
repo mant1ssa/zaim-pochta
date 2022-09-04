bba4e163fd46355a3e119d6d4fdbec3002913a51
mant1ssa <nayrix.8@gmail.com>
2022-09-04 15:46:06 +0500

    all:some changes

--- js/range.js
+++ js/range.js
@@ -4,7 +4,22 @@ const possibility = document.getElementById('pos__sub');
 
 output.innerHTML = slider.value;
 
-// 1000  -  60 000
+console.log(slider);
+
+// 1000  -  100 000
+
+slider.addEventListener("mousemove", function(){
+    var x = slider.value / 1000;
+    var color = 'linear-gradient(90deg, #007aff ' + x + '%, #d9dde0 ' + x + '%)';
+    slider.style.background = color;
+    // console.log(x);
+})
+slider.addEventListener("touchmove", function(){
+    var x = slider.value / 1000;
+    var color = 'linear-gradient(90deg, #007aff ' + x + '%, #d9dde0 ' + x + '%)';
+    slider.style.background = color;
+    // console.log(x);
+})
 slider.oninput = function(){
     output.innerHTML = this.value + " &#8381";
     if(this.value >= 0){
